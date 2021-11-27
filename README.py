51d07339d3a44f4d79dc5f0bfae0a64ddd9c8e2c
Alex4gtx <95112008+Alex4gtx@users.noreply.github.com>
2021-11-26 23:27:32 -0300

    Create README.py

---
+++ README.py
@@ -0,0 +1,11 @@
+- 👋 Hi, I’m @Alex4gtx
+- 👀 I’m interested in all programming languages!
+- 🌱 I’m currently learning Python
+- 💞️ I’m looking to collaborate on (nothing yet) lol
+- 📫 How to reach me | all_name = 'Alex Giovani Hirsch'
+                       print(f'search my name in facebook \033[1:33m{all_name}\033[m:')
+
+<!---
+Alex4gtx/Alex4gtx is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
