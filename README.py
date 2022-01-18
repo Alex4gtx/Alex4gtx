5c6c63a6d356a383194749e1f3bcbef9c68dfd98
Alex Giovani Hirsch <95112008+Alex4gtx@users.noreply.github.com>
2022-01-18 03:07:02 -0300

    Update README.py

--- README.py
+++ README.py
@@ -1,6 +1,7 @@
 - 👋 Hi, I’m @Alex4gtx
 - 👀 I’m interested in all programming languages!
-- 🌱 I’m currently learning Python
+- <3 Python3
+- 🌱 I’m currently learning HTML5, CSS3 and GIT
 - 💞️ I’m looking to collaborate on (nothing yet) lol
 - 📫 How to reach me | all_name = 'Alex Giovani Hirsch'
                        print(f'search my name in facebook \033[1:33m{all_name}\033[m:')
