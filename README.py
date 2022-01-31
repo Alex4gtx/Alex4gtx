e33b0dc8e1e06da6189e81315a5f0004018dd35f
Alex Giovani Hirsch <alexgiovani35@gmail.com>
2022-01-31 04:10:33 -0300

    Update README.py

--- README.py
+++ README.py
@@ -6,6 +6,8 @@
 - 📫 How to reach me | all_name = 'Alex Giovani Hirsch'
                        print(f'search my name in facebook \033[1:33m{all_name}\033[m:')
 
+  <a href="#" rel="teste">teste</a>
+  
 <!---
 Alex4gtx/Alex4gtx is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
 You can click the Preview link to take a look at your changes.
