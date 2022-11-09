eae6729af179cc884fab59b9cb16be7da00a24d5
Alex Giovani Hirsch <alexgiovani35@gmail.com>
2022-11-08 22:38:02 -0300

    Update README.py

--- README.py
+++ README.py
@@ -1,14 +1,3 @@
-- 👋 Hi, I’m @Alex4gtx
-- 👀 I’m interested in all programming languages!
+- 👋 Olá me chamo Alex
 - <3 Python3
-- 🌱 I’m currently learning HTML5, CSS3 and GIT
-- 💞️ I’m looking to collaborate on (nothing yet) lol
-- 📫 How to reach me | all_name = 'Alex Giovani Hirsch'
-                       print(f'search my name in facebook \033[1:33m{all_name}\033[m:')
-
-  <a href="#" rel="teste">teste</a>
-  
-<!---
-Alex4gtx/Alex4gtx is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+- 🌱 Estou estudando recentemente HTML5, CSS3, Javascript e GIT
